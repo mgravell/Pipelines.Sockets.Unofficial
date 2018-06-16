4e68c832c966b6c6cc2579546be0de22b56da260
Marc Gravell <marc.gravell@gmail.com>
2018-06-16 17:44:08 +0100

    experimental sync API

---
+++ src/Pipelines.Sockets.Unofficial/SocketConnection.Sync.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO.Pipelines;
+using System.Net.Sockets;
+
+namespace Pipelines.Sockets.Unofficial
+{
+    interface ISyncDuplexPipe : IDuplexPipe
+    {
+        int Read();
+    }
+    partial class SocketConnection : ISyncDuplexPipe
+    {
+        int ISyncDuplexPipe.Read()
+        {
+            if (ZeroLengthReads && Socket.Available == 0)
+            {
+                DebugLog($"sync zero-length receive...");
+                Receive(Socket, default);
+                DebugLog($"zero-length receive complete; now {Socket.Available} bytes available");
+            }
+            var buffer = _receive.Writer.GetMemory(1);
+            DebugLog($"leased {buffer.Length} bytes from pool");
+
+            DebugLog($"awaiting socket receive...");
+            var bytesReceived = Receive(Socket, buffer);
+            DebugLog($"received {bytesReceived} bytes");
+
+            if (bytesReceived > 0)
+            {
+                _receive.Writer.Advance(bytesReceived);
+            }
+
+            // should I flush here? if I don't flush, is it readable?
+            return bytesReceived;
+        }
+
+        private static int Receive(Socket socket, Memory<byte> buffer)
+        {
+
+#if NETCOREAPP2_1
+            return socket.Receive(buffer.Span);
+#else
+            if (buffer.IsEmpty)
+            {
+                return socket.Receive(Array.Empty<byte>());
+            }
+            else
+            {
+                var segment = buffer.GetArray();
+                return socket.Receive(segment.Array, segment.Offset, segment.Count, SocketFlags.None);
+            }
+#endif
+        }
+
+    }
+}
