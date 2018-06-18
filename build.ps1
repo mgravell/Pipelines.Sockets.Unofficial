e2af0a43d0e82561642e5ed266504d5bd6384954
Nick Craver <craver@stackoverflow.com>
2018-06-18 08:04:51 -0400

    Add build with testing enabled, also creates packages by default
    Since it's so fast: defaults for all.

---
+++ build.ps1
@@ -0,0 +1,62 @@
+[CmdletBinding(PositionalBinding=$false)]
+param(
+    [bool] $CreatePackages = $true,
+    [bool] $RunTests = $true,
+    [string] $PullRequestNumber
+)
+
+Write-Host "Run Parameters:" -ForegroundColor Cyan
+Write-Host "  CreatePackages: $CreatePackages"
+Write-Host "  RunTests: $RunTests"
+Write-Host "  dotnet --version:" (dotnet --version)
+
+$packageOutputFolder = "$PSScriptRoot\.nupkgs"
+$projectsToBuild =
+    'Pipelines.Sockets.Unofficial'
+
+$testsToRun =
+    'Pipelines.Sockets.Unofficial.Tests'
+
+if ($PullRequestNumber) {
+    Write-Host "Building for a pull request (#$PullRequestNumber), skipping packaging." -ForegroundColor Yellow
+    $CreatePackages = $false
+}
+
+Write-Host "Building solution..." -ForegroundColor "Magenta"
+dotnet restore ".\Pipelines.Sockets.Unofficial.sln" /p:CI=true
+dotnet build ".\Pipelines.Sockets.Unofficial.sln" -c Release /p:CI=true
+Write-Host "Done building." -ForegroundColor "Green"
+
+if ($RunTests) {
+    foreach ($project in $testsToRun) {
+        Write-Host "Running tests: $project (all frameworks)" -ForegroundColor "Magenta"
+        Push-Location ".\tests\$project"
+
+        dotnet xunit -configuration Release
+        if ($LastExitCode -ne 0) {
+            Write-Host "Error with tests, aborting build." -Foreground "Red"
+            Pop-Location
+            Exit 1
+        }
+
+        Write-Host "Tests passed!" -ForegroundColor "Green"
+	    Pop-Location
+    }
+}
+
+if ($CreatePackages) {
+    mkdir -Force $packageOutputFolder | Out-Null
+    Write-Host "Clearing existing $packageOutputFolder..." -NoNewline
+    Get-ChildItem $packageOutputFolder | Remove-Item
+    Write-Host "done." -ForegroundColor "Green"
+
+    Write-Host "Building all packages" -ForegroundColor "Green"
+
+    foreach ($project in $projectsToBuild) {
+        Write-Host "Packing $project (dotnet pack)..." -ForegroundColor "Magenta"
+        dotnet pack ".\src\$project\$project.csproj" --no-build -c Release /p:PackageOutputPath=$packageOutputFolder /p:NoPackageAnalysis=true /p:CI=true
+        Write-Host ""
+    }
+}
+
+Write-Host "Done."
