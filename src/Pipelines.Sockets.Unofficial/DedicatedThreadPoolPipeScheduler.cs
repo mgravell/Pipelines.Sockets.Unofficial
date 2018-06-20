86d30cbda49ee766756303741715ce6e1e5bcd1f
Marc Gravell <marc.gravell@gmail.com>
2018-06-20 12:14:41 +0100

    unused

--- src/Pipelines.Sockets.Unofficial/DedicatedThreadPoolPipeScheduler.cs
+++ src/Pipelines.Sockets.Unofficial/DedicatedThreadPoolPipeScheduler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO.Pipelines;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading;
 
 namespace Pipelines.Sockets.Unofficial
