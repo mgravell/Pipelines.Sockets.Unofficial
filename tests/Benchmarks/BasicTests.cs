dae4efd5b3af3336611d3c63f0700909f57caa8b
Nick Craver <nrcraver@gmail.com>
2018-06-24 20:12:31 -0400

    Initial Benchmarks commit
    Note: this runs CLR job to demonstrate the binding failure on System.Buffers ASAP.

---
+++ tests/Benchmarks/BasicTests.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using Pipelines.Sockets.Unofficial.Tests;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    [KeepBenchmarkFiles]
+    [Config(typeof(Config))]
+    public class BasicTests
+    {
+        public static PingPongTests PPTests { get; } = new PingPongTests(TextWriter.Null);
+
+        [Benchmark(Baseline = true, Description = "Socket=>Pipelines=>PingPong")]
+        public Task BasicPipelines() => PPTests.Basic_Pipelines_PingPong();
+
+        [Benchmark(Description = "Socket=>NetworkStream=>PingPong")]
+        public Task BasicNetworkStream() => PPTests.Basic_NetworkStream_PingPong();
+
+        [Benchmark(Description = "Socket=>Pipelines=>TRW=>PingPong")]
+        public Task BasicPipelinesText() => PPTests.Basic_Pipelines_Text_PingPong();
+
+        [Benchmark(Description = "Socket=>NetworkStream=>TRW=>PingPong")]
+        public Task BasicNetworkStreamText() => PPTests.Basic_NetworkStream_Text_PingPong();
+
+        [Benchmark(Description = "Socket=>NetworkStream=>Pipelines=>PingPong")]
+        public Task BasicNetworkStreamPipelines() => PPTests.Basic_NetworkStream_Pipelines_PingPong();
+    }
+}
---
+++ tests/Benchmarks/TLSTests.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using Pipelines.Sockets.Unofficial.Tests;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    [Config(typeof(Config))]
+    public class TLSTests
+    {
+        public static PingPongTests PPTests { get; } = new PingPongTests(TextWriter.Null);
+
+        [Benchmark(Description = "Socket=>Pipelines=>Inverter=>SslStream=>Inverter=>PingPong")]
+        public Task BasicPipelines() => PPTests.ServerClientDoubleInverted_SslStream_PingPong();
+
+        [Benchmark(Description = "Socket=>NetworkStream=>SslStream=>PingPong")]
+        public Task BasicNetworkStream() => PPTests.ServerClient_SslStream_PingPong();
+
+        [Benchmark(Description = "Socket=>NetworkStream=>SslStream=>Inverter=>PingPong")]
+        public Task BasicPipelinesText() => PPTests.ServerClient_SslStream_Inverter_PingPong();
+    }
+}
