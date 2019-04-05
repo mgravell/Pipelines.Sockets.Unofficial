e374065609512dad56b83dae7fac3e7b203b64c9
Marc Gravell <marc.gravell@gmail.com>
2019-04-05 07:09:20 +0100

    spike API for enumerating multicast delegates without allocating
    |                   Method |  Job | Runtime |        Categories |       Mean |     Error |    StdDev |     Median |        Min |        Max | Gen 0/1k Op | Gen 1/1k Op | Gen 2/1k Op | Allocated Memory/Op |
    |------------------------- |----- |-------- |------------------ |-----------:|----------:|----------:|-----------:|-----------:|-----------:|------------:|------------:|------------:|--------------------:|
    |    GetInvocationList_Nil |  Clr |     Clr | GetInvocationList |  0.9083 ns | 0.0175 ns | 0.0172 ns |  0.9080 ns |  0.8741 ns |  0.9494 ns |           - |           - |           - |                   - |
    | GetInvocationList_Single |  Clr |     Clr | GetInvocationList | 20.1373 ns | 0.4014 ns | 0.6009 ns | 20.0998 ns | 19.1682 ns | 21.4336 ns |      0.0102 |           - |           - |                   - |
    |   GetInvocationList_Dual |  Clr |     Clr | GetInvocationList | 43.9891 ns | 0.7859 ns | 1.1271 ns | 44.1548 ns | 41.6279 ns | 46.4498 ns |      0.0127 |           - |           - |                   - |
    |    GetInvocationList_Nil | Core |    Core | GetInvocationList |  0.8491 ns | 0.0148 ns | 0.0139 ns |  0.8515 ns |  0.8238 ns |  0.8687 ns |           - |           - |           - |                   - |
    | GetInvocationList_Single | Core |    Core | GetInvocationList | 17.9527 ns | 0.2356 ns | 0.2204 ns | 17.9975 ns | 17.4232 ns | 18.2188 ns |      0.0102 |           - |           - |                32 B |
    |   GetInvocationList_Dual | Core |    Core | GetInvocationList | 35.7016 ns | 0.4521 ns | 0.4229 ns | 35.8952 ns | 34.8234 ns | 36.1788 ns |      0.0127 |           - |           - |                40 B |
    |                          |      |         |                   |            |           |           |            |            |            |             |             |             |                     |
    |        GetEnumerator_Nil |  Clr |     Clr |     GetEnumerator |  0.6070 ns | 0.0115 ns | 0.0123 ns |  0.6078 ns |  0.5808 ns |  0.6276 ns |           - |           - |           - |                   - |
    |     GetEnumerator_Single |  Clr |     Clr |     GetEnumerator | 19.0600 ns | 0.3725 ns | 0.3985 ns | 18.9460 ns | 18.3617 ns | 19.8927 ns |           - |           - |           - |                   - |
    |       GetEnumerator_Dual |  Clr |     Clr |     GetEnumerator | 33.2096 ns | 0.7430 ns | 1.4666 ns | 32.5588 ns | 31.4005 ns | 38.6445 ns |           - |           - |           - |                   - |
    |        GetEnumerator_Nil | Core |    Core |     GetEnumerator |  0.5691 ns | 0.0078 ns | 0.0073 ns |  0.5725 ns |  0.5568 ns |  0.5761 ns |           - |           - |           - |                   - |
    |     GetEnumerator_Single | Core |    Core |     GetEnumerator | 20.5520 ns | 0.0895 ns | 0.0699 ns | 20.5560 ns | 20.4245 ns | 20.6529 ns |           - |           - |           - |                   - |
    |       GetEnumerator_Dual | Core |    Core |     GetEnumerator | 30.2029 ns | 0.3603 ns | 0.3008 ns | 30.2168 ns | 29.8620 ns | 30.9639 ns |           - |           - |           - |                   - |

---
+++ src/Pipelines.Sockets.Unofficial/Delegates.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+
+namespace Pipelines.Sockets.Unofficial
+{
+    /// <summary>
+    /// Provides utility methods for working with delegates
+    /// </summary>
+    public static class Delegates
+    {
+        /// <summary>
+        /// Iterate over the individual elements of a multicast delegate (without allocation)
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DelegateEnumerator<T> GetEnumerator<T>(this T handler) where T : MulticastDelegate
+            => handler == null ? default : new DelegateEnumerator<T>(handler);
+
+        /// <summary>
+        /// Iterate over the individual elements of a multicast delegate (without allocation)
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static DelegateEnumerable<T> AsEnumerable<T>(this T handler) where T : MulticastDelegate
+            => new DelegateEnumerable<T>(handler);
+
+        private static readonly Func<MulticastDelegate, object> _getArr = GetGetter<object>("_invocationList");
+        private static readonly Func<MulticastDelegate, IntPtr> _getCount = GetGetter<IntPtr>("_invocationCount");
+        private static readonly bool _isOptimized = _getArr != null & _getCount != null;
+
+        internal static bool IsOptimized => _isOptimized;
+
+        private static Func<MulticastDelegate, T> GetGetter<T>(string fieldName)
+        {
+            try
+            {
+                var field = typeof(MulticastDelegate).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+                if (field == null || field.FieldType != typeof(T)) return null;
+#if !NETSTANDARD2_0
+                try // we can try use ref-emit
+                {
+                    var dm = new DynamicMethod(fieldName, typeof(T), new[] { typeof(MulticastDelegate) }, typeof(MulticastDelegate), true);
+                    var il = dm.GetILGenerator();
+                    il.Emit(OpCodes.Ldarg_0);
+                    il.Emit(OpCodes.Ldfld, field);
+                    il.Emit(OpCodes.Ret);
+                    return (Func<MulticastDelegate, T>)dm.CreateDelegate(typeof(Func<MulticastDelegate, T>));
+                }
+                catch { }
+#endif
+                return GetViaReflection<T>(field);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        static Func<MulticastDelegate, T> GetViaReflection<T>(FieldInfo field)
+            => handler => (T)field.GetValue(handler);
+
+        /// <summary>
+        /// Allows allocation-free enumerator over the individual elements of a multicast delegate
+        /// </summary>
+        public readonly struct DelegateEnumerable<T> : IEnumerable<T> where T : MulticastDelegate
+        {
+            private readonly T _handler;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal DelegateEnumerable(T handler) => _handler = handler;
+
+            /// <summary>
+            /// Iterate over the individual elements of a multicast delegate (without allocation)
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public DelegateEnumerator<T> GetEnumerator()
+                => _handler == null ? default : new DelegateEnumerator<T>(_handler);
+            IEnumerator<T> IEnumerable<T>.GetEnumerator() => GetEnumerator();
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        /// <summary>
+        /// Allows allocation-free enumerator over the individual elements of a multicast delegate
+        /// </summary>
+        public struct DelegateEnumerator<T> : IEnumerator<T> where T : MulticastDelegate
+        {
+            private readonly T _handler;
+            private readonly object[] _arr;
+            private readonly int _count;
+            private int _index;
+            private T _current;
+            internal DelegateEnumerator(T handler)
+            {
+                Debug.Assert(handler != null);
+                _handler = handler;
+                if (_isOptimized)
+                {
+                    _arr = (object[])_getArr(handler);
+                    if (_arr == null)
+                    {
+                        _count = 1;
+                    }
+                    else
+                    {
+                        _count = (int)_getCount(handler);
+                    }
+                }
+                else
+                {
+                    _arr = handler.GetInvocationList();
+                    _count = _arr.Length;
+                }
+                _current = null;
+                _index = -1;
+            }
+
+            /// <summary>
+            /// Provides the current value of the seqyence
+            /// </summary>
+            public T Current
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get => _current;
+            }
+
+            object IEnumerator.Current => Current;
+
+            void IDisposable.Dispose() { }
+
+            /// <summary>
+            /// Move to the next item in the sequence
+            /// </summary>
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext()
+            {
+                var next = _index + 1;
+                if (next >= _count)
+                {
+                    _current = null;
+                    return false;
+                }
+                _current = _arr == null ? _handler : (T)_arr[next];
+                _index = next;
+                return true;
+            }
+
+            /// <summary>
+            /// Reset the enumerator, allowing the sequence to be repeated
+            /// </summary>
+            public void Reset()
+            {
+                _current = null;
+                _index = -1;
+            }
+        }
+    }
+}
