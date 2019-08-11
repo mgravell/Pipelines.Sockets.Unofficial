ee8da2b85c9e28601568977bf289f6e9f48ccaf7
Marc Gravell <marc.gravell@gmail.com>
2019-08-11 10:21:11 +0100

    intellisense for pipe scheduler extensions

--- src/Pipelines.Sockets.Unofficial/PipeSchedulerExtensions.cs
+++ src/Pipelines.Sockets.Unofficial/PipeSchedulerExtensions.cs
@@ -4,22 +4,41 @@ using System.Runtime.CompilerServices;
 
 namespace Pipelines.Sockets.Unofficial
 {
+    /// <summary>
+    /// Extension methods for PipeScheduler
+    /// </summary>
     public static class PipeSchedulerExtensions
     {
+        /// <summary>
+        /// Asynchronously yield to the pipe scheduler
+        /// </summary>
         public static YieldAwaitable Yield(this PipeScheduler scheduler)
             => new YieldAwaitable(scheduler);
 
+        /// <summary>
+        /// Enables yielding to a pipe scheduler
+        /// </summary>
         public readonly struct YieldAwaitable : INotifyCompletion, ICriticalNotifyCompletion
         {
             private readonly PipeScheduler _scheduler;
             internal YieldAwaitable(PipeScheduler scheduler)
                 => _scheduler = scheduler == PipeScheduler.Inline ? null : scheduler;
 
+            /// <summary>
+            /// Indicates whether this operation completed synchronously
+            /// </summary>
             public bool IsCompleted => _scheduler == null;
 
+            /// <summary>
+            /// Gets the awaiter for this operation
+            /// </summary>
             public YieldAwaitable GetAwaiter() => this;
 
+            /// <summary>
+            /// Gets the result of this operation
+            /// </summary>
             public void GetResult() { }
+
             private void Schedule(Action continuation)
             {
                 if (continuation == null) return;
