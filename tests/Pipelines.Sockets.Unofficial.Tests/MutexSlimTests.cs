f3f4d4127c62de2cd7700882fe741e17e3fa1e51
Marc Gravell <marc.gravell@gmail.com>
2023-05-03 15:23:59 +0100

    and sync

--- tests/Pipelines.Sockets.Unofficial.Tests/MutexSlimTests.cs
+++ tests/Pipelines.Sockets.Unofficial.Tests/MutexSlimTests.cs
@@ -632,7 +632,7 @@ namespace Pipelines.Sockets.Unofficial.Tests
         public void DuelingThreadsShouldNotStall(int workerCount, int perWorker)
         {
 #if DEBUG
-            perWorker /= 100;
+            perWorker /= 1000;
 #endif
             Volatile.Write(ref _failCount, 0);
             Volatile.Write(ref _successCount, 0);
